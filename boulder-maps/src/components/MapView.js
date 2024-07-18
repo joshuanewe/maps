12b9116cd796fd9bd0e61657ce5103ba3e427046
YabloYescobar <joshuanewe@gmail.com>
2024-07-18 04:20:34 -0700

    added the search component to the map component

--- boulder-maps/src/components/MapView.js
+++ boulder-maps/src/components/MapView.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Map, { Marker, Popup } from "react-map-gl";
 
+import Search from "./Search";
+
 const MAPBOX_TOKEN =
 	"pk.eyJ1Ijoiam9zaHVhbmV3ZSIsImEiOiJjbHlyNWlnNTMwM2ZrMmpvaTdjYnR1Y2M5In0.cPs-Jxueeo6tNBkbNFy1HA"; // Replace with your Mapbox token
 
@@ -42,48 +44,53 @@ const MapView = () => {
 	}, []);
 
 	return (
-		<Map
-			mapboxAccessToken={MAPBOX_TOKEN}
-			initialViewState={{
-				longitude: -122.4,
-				latitude: 37.8,
-				zoom: 14,
-			}}
-			style={{ width: 600, height: 400 }}
-			mapStyle="mapbox://styles/mapbox/streets-v9"
-		>
-			{climbs.map((climb) => (
-				<Marker
-					key={climb.id}
-					latitude={climb.latitude}
-					longitude={climb.longitude}
-				>
-					<button
-						className="marker-btn"
-						onClick={(e) => {
-							e.preventDefault();
-							setSelectedClimb(climb);
-						}}
+		<div className="map-container">
+			<Map
+				mapboxAccessToken={MAPBOX_TOKEN}
+				initialViewState={{
+					longitude: -122.4,
+					latitude: 37.8,
+					zoom: 14,
+				}}
+				style={{ width: 600, height: 400 }}
+				mapStyle="mapbox://styles/mapbox/streets-v9"
+			>
+				{climbs.map((climb) => (
+					<Marker
+						key={climb.id}
+						latitude={climb.latitude}
+						longitude={climb.longitude}
 					>
-						📍
-					</button>
-				</Marker>
-			))}
+						<button
+							className="marker-btn"
+							onClick={(e) => {
+								e.preventDefault();
+								setSelectedClimb(climb);
+							}}
+						>
+							📍
+						</button>
+					</Marker>
+				))}
 
-			{selectedClimb && (
-				<Popup
-					latitude={selectedClimb.latitude}
-					longitude={selectedClimb.longitude}
-					onClose={() => setSelectedClimb(null)}
-				>
-					<div>
-						<h2>{selectedClimb.name}</h2>
-						<p>Grade: {selectedClimb.grade}</p>
-						<p>Stars: {selectedClimb.stars}</p>
-					</div>
-				</Popup>
-			)}
-		</Map>
+				{selectedClimb && (
+					<Popup
+						latitude={selectedClimb.latitude}
+						longitude={selectedClimb.longitude}
+						onClose={() => setSelectedClimb(null)}
+					>
+						<div>
+							<h2>{selectedClimb.name}</h2>
+							<p>Grade: {selectedClimb.grade}</p>
+							<p>Stars: {selectedClimb.stars}</p>
+						</div>
+					</Popup>
+				)}
+			</Map>
+			<div className="search-container">
+				<Search />
+			</div>
+		</div>
 	);
 };
 
